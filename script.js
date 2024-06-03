17e65b6d686a73e089189a14af0a714f1feb83ba
TheKiruls <amirin856@gmail.com>
2024-06-03 23:42:57 +0800

    javascript

---
+++ script.js
@@ -0,0 +1,40 @@
+const instagram = document.querySelector(".instagram");
+const linkedIn = document.querySelector(".linkedin");
+const gitHub = document.querySelector(".github");
+const gmail = document.querySelector(".gmail");
+
+// function instaOpen() {
+//     window.open("https://www.instagram.com/khairulamirn/");
+// }; 
+
+// function linkedInOpen() {
+//     window.open("https://www.linkedin.com/in/khairulamirin-lamin-69443123b/");
+// };
+
+// function gitHubOpen() {
+//     window.open("https://github.com/khairulamirn");
+// };
+
+// function gmailOpen() {
+//     window.open("mailto:khairulamirin.lamin@gmail.com");
+// };
+
+// instagram.addEventListener ("click", function () {
+//     instaOpen();
+//     console.log("Instagram open");
+// });
+
+// linkedIn.addEventListener ("click", function () {
+//     linkedInOpen();
+//     console.log("linkedIn open");
+// });
+
+// gitHub.addEventListener ("click", function () {
+//     gitHubOpen();
+//     console.log("gitHub open");
+// });
+
+// gmail.addEventListener ("click", function () {
+//     gmailOpen();
+//     console.log("gmail open");
+// });
