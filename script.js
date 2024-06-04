c5bbc348280ab0df6159c4c5878d86787eb65e1e
TheKiruls <amirin856@gmail.com>
2024-06-04 23:49:26 +0800

    updated

--- script.js
+++ script.js
@@ -1,40 +1,53 @@
+// social media 
 const instagram = document.querySelector(".instagram");
 const linkedIn = document.querySelector(".linkedin");
 const gitHub = document.querySelector(".github");
 const gmail = document.querySelector(".gmail");
 
-// function instaOpen() {
-//     window.open("https://www.instagram.com/khairulamirn/");
-// }; 
+// projects 
+const button = {
+    pacman: document.querySelector("#button1"),
+    taskApp: document.querySelector("#button2"),
+    quote: document.querySelector("#button3"),
+    randomPassword: document.querySelector("#button4"),
+};
 
-// function linkedInOpen() {
-//     window.open("https://www.linkedin.com/in/khairulamirin-lamin-69443123b/");
-// };
+// projects click event 
+button.pacman.addEventListener ("click", function () {
+    window.open("https://github.com/khairulamirn/W3-Day-9/tree/main/pacman");
+});
 
-// function gitHubOpen() {
-//     window.open("https://github.com/khairulamirn");
-// };
+button.taskApp.addEventListener ("click", function () {
+    window.open("https://github.com/khairulamirn/JavaScript-Exercise/tree/main/Quote%20Generator");
+});
 
-// function gmailOpen() {
-//     window.open("mailto:khairulamirin.lamin@gmail.com");
-// };
+button.quote.addEventListener ("click", function () {
+    window.open("https://github.com/khairulamirn/JavaScript-Exercise/tree/main/Quote%20Generator");
+});
 
-// instagram.addEventListener ("click", function () {
-//     instaOpen();
-//     console.log("Instagram open");
-// });
+button.randomPassword.addEventListener ("click", function () {
+    window.open("https://github.com/khairulamirn/JavaScript-Exercise/tree/main/Random%20Password%20Generator");
+});
 
-// linkedIn.addEventListener ("click", function () {
-//     linkedInOpen();
-//     console.log("linkedIn open");
-// });
 
-// gitHub.addEventListener ("click", function () {
-//     gitHubOpen();
-//     console.log("gitHub open");
-// });
 
-// gmail.addEventListener ("click", function () {
-//     gmailOpen();
-//     console.log("gmail open");
-// });
+// social media click event
+instagram.addEventListener ("click", function () {
+    window.open("https://www.instagram.com/khairulamirn/");
+    console.log("Instagram open");
+});
+
+linkedIn.addEventListener ("click", function () {
+    window.open("https://www.linkedin.com/in/khairulamirin-lamin-69443123b/");
+    console.log("linkedIn open");
+});
+
+gitHub.addEventListener ("click", function () {
+    window.open("https://github.com/khairulamirn");
+    console.log("gitHub open");
+});
+
+gmail.addEventListener ("click", function () {
+    window.open("mailto:khairulamirin.lamin@gmail.com");
+    console.log("gmail open");
+});
